21b8408c1223d85ca57f5508192d716ecd25dc14
Sudhanshu Kulkarni <sudhanshu.kulkarni50@gmail.com>
2026-03-07 11:27:59 +0530

    Added Logic to fetch last 7 days CPU metrics Data using CloudWatch

--- Lambda_Cloud_Control_Function.py
+++
@@ -1,5 +0,0 @@
-import boto3
-from datetime import datetime, timedelta
-
-ec2 = boto3.client('ec2')
-cloudwatch = boto3.client('cloudwatch')
---
+++ Lambda_Control_Function.py
@@ -0,0 +1,42 @@
+import boto3
+from datetime import datetime, timedelta
+
+ec2 = boto3.client('ec2')
+cloudwatch = boto3.client('cloudwatch')
+
+INSTANCE_ID = "i-022d26bb6f22a5bad"
+
+def get_cpu_metrics():
+
+    end_time = date_time.utcnow()
+    start_time = end_time - timedelta(days=7)
+
+    response = cloudwatch.get_metric_statistics(
+        Namespace='AWS/EC2',
+        MetricName='CPUUtilization',
+        Dimensions=[
+            {
+                'Name': 'InstanceId',
+                'Value': INSTANCE_ID
+            },
+        ],
+        StartTime=start_time,
+        EndTime=end_time,
+        Period=3600,
+        Statistics=['Average']
+    )
+    datapoints = response['Datapoints']
+
+    if not datapoints:
+        return "No data"
+
+    avg_cpu = sum([point['Average'] for point in datapoints]) / len(datapoints)
+
+    return round(avg_cpu,2)
+
+def lambda_handler(event, context):
+
+    cpu_avg = get_cpu_metric()
+    return {
+        "Average_CPU_Last_7_Days": cpu_avg
+    }
