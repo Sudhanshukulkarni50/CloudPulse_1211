3827f379d618dd42ec804875c6cc24948b7f0c8f
Sudhanshu Kulkarni <sudhanshu.kulkarni50@gmail.com>
2026-03-12 17:06:53 +0530

    Added Some files

---
+++ CloudPulse-Instance-Control-Function.py
@@ -0,0 +1,84 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fcfa21f6-9f59-4941-86d8-501cd4981ff5",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import json\n",
+    "import boto3\n",
+    "\n",
+    "ec2 = boto3.client('ec2')\n",
+    "\n",
+    "def lambda_handler(event, context):\n",
+    "\n",
+    "    params = event.get('queryStringParameters', {})\n",
+    "\n",
+    "    instance_id = params.get('instance_id')\n",
+    "    action = params.get('action')\n",
+    "\n",
+    "    if not instance_id or not action:\n",
+    "        return {\n",
+    "            \"statusCode\": 400,\n",
+    "            \"body\": json.dumps(\"Missing instance_id or action\")\n",
+    "        }\n",
+    "\n",
+    "    try:\n",
+    "\n",
+    "        if action == \"start\":\n",
+    "            ec2.start_instances(InstanceIds=[instance_id])\n",
+    "            message = f\"Instance {instance_id} started successfully\"\n",
+    "\n",
+    "        elif action == \"stop\":\n",
+    "            ec2.stop_instances(InstanceIds=[instance_id])\n",
+    "            message = f\"Instance {instance_id} stopped successfully\"\n",
+    "\n",
+    "        else:\n",
+    "            return {\n",
+    "                \"statusCode\": 400,\n",
+    "                \"body\": json.dumps(\"Invalid action\")\n",
+    "            }\n",
+    "\n",
+    "        return {\n",
+    "            \"statusCode\": 200,\n",
+    "            \"headers\": {\n",
+    "                \"Access-Control-Allow-Origin\": \"*\"\n",
+    "            },\n",
+    "            \"body\": json.dumps({\n",
+    "                \"message\": message\n",
+    "            })\n",
+    "        }\n",
+    "\n",
+    "    except Exception as e:\n",
+    "\n",
+    "        return {\n",
+    "            \"statusCode\": 500,\n",
+    "            \"body\": json.dumps(str(e))\n",
+    "        }"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.14.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
--- Lambda_Control_Function.py
+++ Lambda_Control_Function.py
@@ -1,20 +1,38 @@
 import boto3
 from datetime import datetime, timedelta
 
-sns = boto3.client('sns')
-SNS_TOPIC_ARN = "arn:aws:sns:ap-south-1:504115868738:CloudPulse_Alerts"
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('CloudPulse_Insights')
-
 ec2 = boto3.client('ec2')
 cloudwatch = boto3.client('cloudwatch')
+sns = boto3.client('sns')
+dynamodb = boto3.resource('dynamodb')
 
-INSTANCE_ID = "i-022d26bb6f22a5bad"
+SNS_TOPIC_ARN = "arn:aws:sns:ap-south-1:504115868738:CloudPulse_Alerts"
+table = dynamodb.Table('CloudPulse_Insights')
 
-def get_cpu_metrics():
 
-    end_time = date_time.utcnow()
+def get_running_instances():
+    
+    response = ec2.describe_instances(
+        Filters=[
+            {
+                'Name': 'instance-state-name',
+                'Values': ['running']
+            }
+        ]
+    )
+
+    instance_ids = []
+
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_ids.append(instance['InstanceId'])
+
+    return instance_ids
+
+
+def get_cpu_metrics(instance_id):
+
+    end_time = datetime.utcnow()
     start_time = end_time - timedelta(days=7)
 
     response = cloudwatch.get_metric_statistics(
@@ -23,26 +41,29 @@ def get_cpu_metrics():
         Dimensions=[
             {
                 'Name': 'InstanceId',
-                'Value': INSTANCE_ID
-            },
+                'Value': instance_id
+            }
         ],
         StartTime=start_time,
         EndTime=end_time,
         Period=3600,
         Statistics=['Average']
     )
+
     datapoints = response['Datapoints']
 
     if not datapoints:
-        return "No data"
+        return 0
 
     avg_cpu = sum([point['Average'] for point in datapoints]) / len(datapoints)
 
-    return round(avg_cpu,2)
+    return round(avg_cpu, 2)
 
-def get_recent_cpu_metrics():
+
+def get_recent_cpu_metrics(instance_id):
+
     end_time = datetime.utcnow()
-    start_time = end_time - timedelta(hours = 24)
+    start_time = end_time - timedelta(hours=24)
 
     response = cloudwatch.get_metric_statistics(
         Namespace='AWS/EC2',
@@ -50,8 +71,8 @@ def get_recent_cpu_metrics():
         Dimensions=[
             {
                 'Name': 'InstanceId',
-                'Value': INSTANCE_ID
-            },
+                'Value': instance_id
+            }
         ],
         StartTime=start_time,
         EndTime=end_time,
@@ -61,7 +82,9 @@ def get_recent_cpu_metrics():
 
     return response['Datapoints']
 
+
 def predict_future_cpu(datapoints):
+
     if not datapoints:
         return 0
 
@@ -73,62 +96,77 @@ def predict_future_cpu(datapoints):
 
     return round(predicted_cpu, 2)
 
-    
+
 def classify_instance(cpu):
 
-    if cpu<20:
-        return "Underutilised", "Should stop instance to save cost"
-    elif cpu<=70:
+    if cpu < 20:
+        return "Underutilized", "Consider stopping instance to save cost"
+
+    elif cpu <= 70:
         return "Healthy", "No action required"
+
     else:
-        return "Overutilised","Should upgrade instance type"
+        return "Overutilized", "Consider upgrading instance type"
+
 
 def lambda_handler(event, context):
 
-    cpu_avg = get_cpu_metrics()
-    classification, recommendation = classify_instance(cpu_avg)
+    instance_ids = get_running_instances()
 
-    recent_data = get_recent_cpu_metrics()
+    results = []
 
-    predicted_cpu = predict_future_cpu(recent_data)
+    for instance_id in instance_ids:
 
-    fromm datetime import datetime
+        cpu_avg = get_cpu_metrics(instance_id)
+        print(f"DEBUG → {instance_id} CPU: {cpu_avg}")
+        classification, recommendation = classify_instance(cpu_avg)
 
-    table.put_item(
-        Item = {
-            "Instance_ID": INSTANCEID,
-            "Timestamp": str(datetime.utcnow()),
-            "Average_CPU_Last_7_Days": str(cpu_avg),
-            "Predicted_CPU": str(predicted_cpu),
+        recent_data = get_recent_cpu_metrics(instance_id)
+
+        predicted_cpu = predict_future_cpu(recent_data)
+
+        table.put_item(
+            Item={
+                "Instance_ID": instance_id,
+                "Timestamp": str(datetime.utcnow()),
+                "Average_CPU_Last_7_Days": str(cpu_avg),
+                "Predicted_CPU": str(predicted_cpu),
+                "Classification": classification,
+                "Recommendation": recommendation
+            }
+        )
+
+        if classification == "Overutilized":
+
+            sns.publish(
+                TopicArn=SNS_TOPIC_ARN,
+                Subject="CloudPulse Alert: EC2 Instance Overutilized",
+                Message=f"""
+CloudPulse Monitoring Alert
+
+Instance ID: {instance_id}
+
+Average CPU (Last 7 Days): {cpu_avg}%
+Predicted CPU (Next Hours): {predicted_cpu}%
+
+Status: Overutilized
+
+Recommendation:
+{recommendation}
+
+Timestamp: {datetime.utcnow()}
+"""
+            )
+
+        results.append({
+            "Instance_ID": instance_id,
+            "Average_CPU_Last_7_Days": cpu_avg,
+            "Predicted_CPU_Next_Hours": predicted_cpu,
             "Classification": classification,
             "Recommendation": recommendation
-        }
-    )
-    if classification == "Overutilized":
-        sns.publish(
-            TopicArn=SNS_TOPIC_ARN,
-            Subject="CloudPulse Alert: EC2 Instance Overutilized",
-            Message=f"""
-               CloudPulse Monitoring Alert
+        })
 
-               Instance ID: {INSTANCE_ID}
-
-               Average CPU (Last 7 Days): {cpu_avg}%
-               Predicted CPU (Next Hours): {predicted_cpu}%
-
-               Status: Overutilized
-
-               Recommendation:
-               {recommendation}
-
-               Timestamp: {datetime.utcnow()}
-           """
-    )
-    
     return {
-        "InstanceID":INSTANCE_ID,
-        "Average_CPU_Last_7_Days": cpu_avg,
-        "Predicted_CPU_Next_Hours": predicted_cpu,
-        "Classification": classification,
-        "Recommendation": recommendation
+        "Total_Instances_Processed": len(results),
+        "Results": results
     }
